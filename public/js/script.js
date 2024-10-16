d30670c6cb93b1bc8d52f0850f479cf9ef1c2d57
pitiphat jadnuang <pitiphat.jad@dome.tu.ac.th>
2024-10-16 22:33:54 +0700

    delete hello button

--- public/js/script.js
+++ public/js/script.js
@@ -18,20 +18,3 @@ function submitLogin() {
 }
 
 
-
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
-    
-    fetch(url)
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('message').innerText = text;
-    })
-    .catch(error => console.error('Error:', error));
-}
--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
