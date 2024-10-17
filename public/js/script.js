41928dd382aec75e4ee5d4f6b1935b7aa2f4e320
pitiphat jadnuang <pitiphat.jad@dome.tu.ac.th>
2024-10-18 04:24:17 +0700

    add show password button

--- public/js/script.js
+++ public/js/script.js
@@ -17,4 +17,13 @@ function submitLogin() {
     .catch(error => console.error('Error:', error));
 }
 
+    function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
 
